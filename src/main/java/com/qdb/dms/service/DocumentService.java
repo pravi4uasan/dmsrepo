9e9fe40f8fa6d434b84d4667d53b6199ca473bbd
pravi4uasan <39478176+pravi4uasan@users.noreply.github.com>
2020-07-25 19:12:19 -0400

    Update DocumentService.java

--- src/main/java/com/qdb/dms/service/DocumentService.java
+++ src/main/java/com/qdb/dms/service/DocumentService.java
@@ -1,6 +1,7 @@
 package com.qdb.dms.service;
 
 import java.io.IOException;
+import java.util.List;
 
 import org.springframework.web.multipart.MultipartFile;
 
@@ -11,5 +12,7 @@ public interface DocumentService {
 	public DocumentUploadResponse uploadDocument(MultipartFile fileRequest) throws IOException;
 	
 	public byte[] downloadDocument(int docId) throws IOException;
+
+	public List<Integer> listDocuments();
 
 }
