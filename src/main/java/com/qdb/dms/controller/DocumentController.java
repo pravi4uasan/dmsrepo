29289b7b9351ec23f79fc2534e1410901c210f44
pravi4uasan <39478176+pravi4uasan@users.noreply.github.com>
2020-07-26 12:47:17 -0400

    Update DocumentController.java

--- src/main/java/com/qdb/dms/controller/DocumentController.java
+++ src/main/java/com/qdb/dms/controller/DocumentController.java
@@ -11,6 +11,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -66,6 +67,11 @@ public class DocumentController {
 	@GetMapping(value = "/documents")
 	public List<Integer> listDocuments() {
 		return documentService.listDocuments();
+	}
+	
+	@DeleteMapping(value = "/documents/{docId}")
+	public void deleteDocument(@PathVariable(value = "docId") int docId) {
+		documentService.deleteDocument(docId);
 	}
 	
 	@PostMapping(value = "/documents/posts", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
